cd004eac7552997bdf5bda0c56066ca1b1546aac
Trevor W <tw@trevorwiebe.com>
2025-11-30 18:48:17 -0600

    add icon library and show heart

--- front-end/src/components/model/ModelItem.tsx
+++ front-end/src/components/model/ModelItem.tsx
@@ -2,20 +2,23 @@ import {numberFormatter} from '../../utils/numberFormat'
 import {dateFormatter} from '../../utils/dateFormat'
 import Chip from '../Chip';
 import { HuggingFaceModel } from '../../types';
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 interface ModelItemProps {
     name: HuggingFaceModel;
+    authenticated: Boolean | undefined
 }
 
 export default function ModelItem(props: ModelItemProps) {
 
     const {_id, id, downloads, likes, createdAt} = props.name;
-
-    console.log(_id, id);
     
     return (
         <div className="rounded-md shadow-sm p-4 m-2">
-            <p className="mb-2">{id}</p>
+            <div className='flex flex-box justify-between'>
+                <p className="mb-2">{id}</p>
+                {props.authenticated && <FaRegHeart/>}
+            </div>
             <div>
                 <Chip label={"Downloads"} text={numberFormatter(downloads)}/>
                 <Chip label={"Likes"} text={numberFormatter(likes)}/>
--- front-end/src/components/model/ModelList.tsx
+++ front-end/src/components/model/ModelList.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useUser } from "@clerk/clerk-react";
+
 import { AppDispatch } from '../../store'
 import { fetchModels } from '../../store'
 import { RootState } from '../../types'
@@ -11,6 +13,8 @@ import Error from '../small/Error';
 export default function ModelsList() {
   const dispatch = useDispatch<AppDispatch>();
   const { data: models, isLoading, error } = useSelector((state: RootState) => state.models);
+
+  const { isSignedIn } = useUser();
 
   useEffect(() =>{
     if(!models.length){
@@ -26,7 +30,7 @@ export default function ModelsList() {
       <ul>
         {models.map(model => (
           <li key={model.id}>
-            <ModelItem name={model}/>
+            <ModelItem name={model} authenticated={isSignedIn}/>
           </li>
         ))}
       </ul>
