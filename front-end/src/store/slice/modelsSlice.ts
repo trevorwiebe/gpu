e6a0712fa1ba1d36574f5ce2ccae8936ee5f4625
Trevor W <tw@trevorwiebe.com>
2025-12-03 12:32:58 -0600

    remove old code

--- front-end/src/store/slice/modelsSlice.ts
+++ front-end/src/store/slice/modelsSlice.ts
@@ -24,30 +24,6 @@ const modelSortSlice = createSlice({
 
             state.sortBy = sortBy;
             state.sortOrder = sortOrder;
-
-            // state.data.sort((a, b) => {
-
-            //     const valueA = a[sortBy as keyof typeof a];
-            //     const valueB = b[sortBy as keyof typeof b];
-
-            //     if(sortBy == "createdAt"){
-            //         if(sortOrder == 'asc'){
-            //             return new Date(valueB as string).getTime() - new Date(valueA as string).getTime()
-            //         }else if(sortOrder == 'desc'){
-            //             return new Date(valueA as string).getTime() - new Date(valueB as string).getTime()
-            //         }else{
-            //             return 0
-            //         }
-            //     }else{
-            //         if(sortOrder == 'asc'){
-            //             return (valueB as number) - (valueA as number)
-            //         }else if(sortOrder == 'desc'){
-            //             return (valueA as number) - (valueB as number)
-            //         }else{
-            //             return 0
-            //         }
-            //     }
-            // });
         },
     },
 });
